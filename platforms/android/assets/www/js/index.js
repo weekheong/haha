3547f51c5cc0ba85e4793d637948f72eacbabfae
weekheongquape <wee.kheong@quape.com>
2015-12-09 22:35:57 +0800

    haha

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		alert("HAHA");
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
